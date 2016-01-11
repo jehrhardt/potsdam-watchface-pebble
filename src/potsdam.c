02a1cdd6a6b352fe48ee2844f6fd04a05c9a75ba
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-01-11 21:04:45 +0100

    Reformat code

--- src/potsdam.c
+++ src/potsdam.c
@@ -6,31 +6,31 @@ static void timer_tick(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void window_load(Window *window) {
-  window_set_background_color(window, GColorDarkCandyAppleRed);
+    window_set_background_color(window, GColorDarkCandyAppleRed);
 }
 
 static void window_unload(Window *window) {
 }
 
 static void init(void) {
-  window = window_create();
+    window = window_create();
 
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload
-  });
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload
+    });
 
-  window_stack_push(window, true);
-  tick_timer_service_subscribe(SECOND_UNIT, timer_tick);
+    window_stack_push(window, true);
+    tick_timer_service_subscribe(SECOND_UNIT, timer_tick);
 }
 
 static void deinit(void) {
-  tick_timer_service_unsubscribe();
-  window_destroy(window);
+    tick_timer_service_unsubscribe();
+    window_destroy(window);
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
